16409a5f2ae87aaac5e3585d80be1bb685803b32
Alex-Nagy <nagyal3x@gmail.com>
2021-12-01 12:10:54 +0100

    form styled

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const inputElement = (type, name, label) => {
   return `
   <div>
+    <label for="${name}">${label}</label>
+    <input type="${type}" name="${name}" id="${name}"">
+  </div>
+    `;
+};
+const selectElement = (type, name, label, selectOptions) => {
+  let optionElements = "";
+  for (const option of selectOptions) {
+    optionElements += `
+      <option>${option}</option>
+    `;
+  }
+  return `
+  <div>
     <label>${label}</label>
-    <input type="${type}" name="${name}">
+    <${type} name='${name}'>
+      ${optionElements}
+    </${type}>
   </div>
     `;
 };
@@ -10,9 +26,14 @@ const inputElement = (type, name, label) => {
 const formElement = `
     <form id="form">
     ${inputElement("text", "firstName", "Keresztneved")}
-    ${inputElement("file", "profilePic", "Profilképed")}
     ${inputElement("email", "personalEmail", "Email címed")}
-    ${inputElement("radio", "newsletter", "Szeretnél hírlevelet?")}
+    ${inputElement("file", "profilePic", "Profilképed")}
+    ${selectElement("select", "where", "Hol hallottál rólunk?", [
+      "internetről",
+      "ismerőstől",
+      "egyéb",
+    ])}
+    ${inputElement("checkbox", "newsletter", "Szeretnél hírlevelet?")}
     ${inputElement("checkbox", "terms", "Elfogadod a feltételeket?")}
     <button>OK</button>
     </form>
@@ -20,25 +41,35 @@ const formElement = `
 
 const formSubmit = (event) => {
   event.preventDefault();
-  console.log(event);
-  event.target.classList.add("submitted");
+  // console.log(event);
+  const et = event.target;
+  et.classList.add("submitted");
+  const etValue = et.querySelector(`select[name="where"]`).value;
+  console.log(etValue);
 };
 
 // todo: ha az aktuális inputnak a name attribútuma "firstName" csak akkor írja bele ebbe a div-be a tartalmat
+
 const inputEvent = (event) => {
+  let tryForm = event.target.closest("#form");
   console.log(event.target.value);
-  
-  if (event.target.name === "firstName") {
+  // console.log(tryForm);
+
+/*   if (event.target.getAttribute("name") === "firstName") {
     document.getElementById("inputValueContent").innerHTML = event.target.value;
   }
+*/
 };
 
 function loadEvent() {
   let root = document.getElementById("root");
   root.insertAdjacentHTML("beforeend", formElement);
-  root.insertAdjacentHTML("beforeend", `
+  root.insertAdjacentHTML(
+    "beforeend",
+    `
     <div id="inputValueContent"></div>
-  `);
+  `
+  );
 
   let form = document.getElementById("form");
   form.addEventListener("submit", formSubmit);
